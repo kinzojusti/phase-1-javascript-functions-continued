fcfaf02d14437e7d7e3286a2188e0d3dac5e78f1
+ <+>
2022-04-04 17:04:41 -0400

    commit tree 6fe988858caebfd8ab06382aeee6b6788a84d780

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+//In JavaScript, the most common way to define functions is with a FUNCTION DECLARATION:
+// razzle is a pointer. we use the pointer to call or invoke the function by adding () after the pointer.
+
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+  }
+  
+  let mondayWork = function(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+  }
+  
+  let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }}
